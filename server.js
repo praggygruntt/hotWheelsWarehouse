af9a006329462a50182f8257ac8eccbb732d0e26
Zachary Bowman <zchrybwmn@gmail.com>
2019-04-10 11:02:12 -0400

    REDO Setup Get Route for Index of Cars, Small Tweaks

--- server.js
+++ server.js
@@ -13,7 +13,19 @@ const db = mongoose.connect('mongodb://localhost/hotWheelsWarehouse');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-// Routes
+// Routes ============================================
+// Get Full List of Products (Index Route)
+app.get('/cars', function(req, res) {
+    Car.find({}, function(err, returnedCars) {
+        if (err) {
+            res.status(500).send("Internal Server Error")
+        } else {
+            res.status(200).send(returnedCars);
+        };
+    });
+});
+
+// Post a New Car to the Database(and return newly saved Car)  
 app.post('/cars', function(req, res) {
     let newCar = new Car();
     newCar.name = req.body.name;
